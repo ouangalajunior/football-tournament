57e85a5f02e4bc8156d6660e9219342cba9ccbab
Junior Ouangala <ouangalajunior@MacBook-Air-de-Junior.local>
2024-02-13 09:47:13 +0100

    Project starter

---
+++ src/test/java/cas/rad/springboot/footballtournament/FootballTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package cas.rad.springboot.footballtournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
